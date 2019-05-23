ce22c540b5bdd450898353f6ee8b9634c1c3a18b
Milind Oka <oak444@gmail.com>
2019-05-23 18:03:49 +0530

    Loaded Single Batch For Combined PDF testing. to be added in loop

--- app/src/main/java/ilugbom/org/in/batchmaker/CreateCombinedPDF.java
+++ app/src/main/java/ilugbom/org/in/batchmaker/CreateCombinedPDF.java
@@ -3,8 +3,11 @@ package ilugbom.org.in.batchmaker;
 import android.os.Environment;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FilenameFilter;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class CreateCombinedPDF
@@ -48,18 +51,129 @@ public class CreateCombinedPDF
         int totalfiles=listfiles(rootDir);
         MA.show(totalfiles);
 
-
+        LoadBatch(fileArray.get(0));
 
     }
 
+/////////////// Load Single Batch
 
-    void LoadSingleFile(String file)
+    //boolean NewNow=false,selectall=false,end=false,OpenNow=false;
+    String  BatchNo="01",Date="",BatchTime="",School="SIWS College",Index="J-31.04.005",
+            Strim="Science", Standard="HSC",Subject="Mathematics",SubjectCode="40",Type="Practical",
+            Email1="",Email2="",BatchCreator="MO",BatchSession="";
+
+    String froll,lroll;
+    ArrayList<String> tempRoll = new ArrayList<>();
+
+    void LoadBatch(String fylenamewithpath)
     {
+        try
+        {
+
+            File myFile = new File(fylenamewithpath);
+            FileInputStream fIn = new FileInputStream(myFile);
+            BufferedReader myReader = new BufferedReader(new InputStreamReader(fIn));
+            String aDataRow = "";
 
 
+            aDataRow=myReader.readLine(); /// blank line separator
+
+            String temp[],stemp;
 
 
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            MA.PD.Zone=temp[1].trim();
 
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            School=temp[1].trim();
+
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            Index=temp[1].trim();
+
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            Strim=temp[1].trim();
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            Standard=temp[1].trim();
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            Subject=temp[1].trim();
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            SubjectCode=temp[1].trim();
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            Type=temp[1].trim();
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            BatchNo=temp[1].trim();
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            BatchCreator=temp[1].trim();
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            Email1=temp[1].trim();
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            Email2=temp[1].trim();
+
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            Date=temp[1].trim();
+
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            BatchTime=temp[1].trim();
+
+            stemp= myReader.readLine();
+            temp=stemp.split(":");
+            BatchSession=temp[1].trim();
+
+            aDataRow=myReader.readLine(); /// blank line separator
+            aDataRow=myReader.readLine(); /// ==== Reserved line ===
+            aDataRow=myReader.readLine(); /// blank line separator
+            aDataRow=myReader.readLine(); /// Seat Nos: Tag
+            aDataRow=myReader.readLine(); /// blank line separator
+
+            tempRoll.removeAll(tempRoll);
+            while ((aDataRow = myReader.readLine()) != null)
+
+            {
+                tempRoll.add(aDataRow);
+            }
+
+            myReader.close();
+
+            int tot=tempRoll.size();
+            froll=tempRoll.get(0);
+            lroll=tempRoll.get(tot-1);
+
+            MA.show(froll);MA.show(lroll);
+
+
+            //show("Loaded From SD Card");
+
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(MA,e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
     }
 
 
