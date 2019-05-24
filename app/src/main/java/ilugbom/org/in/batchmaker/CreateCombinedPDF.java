531ae0343f8c97075f1f043c4f4c5f0a27c1e56e
Milind Oka <oak444@gmail.com>
2019-05-24 12:46:35 +0530

    Combined PDF created with header and body and footer for ORAL only worked

--- app/src/main/java/ilugbom/org/in/batchmaker/CreateCombinedPDF.java
+++ app/src/main/java/ilugbom/org/in/batchmaker/CreateCombinedPDF.java
@@ -85,6 +85,7 @@ public class CreateCombinedPDF
                 AddBoxedText(document);
                 AddOralHeader(document);
                 AddORALBody(document);
+                AddORALFooter(document);
                 document.newPage();
             }
 
@@ -454,6 +455,55 @@ public class CreateCombinedPDF
 
         document.add(table2);
 
+    }
+
+
+
+    void AddORALFooter(Document document) throws DocumentException
+    {  float footcolwid[]= {8,6};
+        PdfPTable table = new PdfPTable(footcolwid);
+        table.setWidthPercentage(95);
+        table.setSpacingBefore(40f);
+
+        PdfPCell cell = new PdfPCell(new Phrase("Supervisor's/Teacher's Name",normal));
+        cell.setBorder(PdfPCell.NO_BORDER);
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);
+        table.addCell(cell);
+
+
+        cell = new PdfPCell(new Phrase("Conductor/Principal/Head Master Signature",normal));
+        cell.setBorder(PdfPCell.NO_BORDER);
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);
+        table.addCell(cell);
+
+
+        cell = new PdfPCell(new Phrase("Signature",normal));
+        cell.setBorder(PdfPCell.NO_BORDER);
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);
+        table.addCell(cell);
+
+        cell = new PdfPCell(new Phrase("And Stamp",normal));
+        cell.setBorder(PdfPCell.NO_BORDER);
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);
+        table.addCell(cell);
+
+        table.setSpacingAfter(8f);
+
+        ////////////////////////// TABLE ROW
+
+
+        PdfPTable table2 = new PdfPTable(1);
+        table2.setWidthPercentage(95);
+
+
+        cell = new PdfPCell(new Phrase("Note : To be kept by Center/School/College Conductor " +
+                "for one year after the declaration of the result.",normal));
+        cell.setBorder(PdfPCell.NO_BORDER);
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);cell.setBorder(PdfPCell.NO_BORDER);
+        table2.addCell(cell);
+
+        document.add(table);
+        document.add(table2);
     }
 
 
